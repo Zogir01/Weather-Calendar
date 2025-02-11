1fe39c11f5b201122f92a1a7bb650253b750b349
Tomasz Wojtasek <tom3k2002w@gmail.com>
2025-02-11 15:07:03 +0100

    Final commit before the pass (#2).

--- Weather_Calendar/src/main/java/com/zogirdex/weather_calendar/uiutil/StageAssistant.java
+++ Weather_Calendar/src/main/java/com/zogirdex/weather_calendar/uiutil/StageAssistant.java
@@ -19,11 +19,6 @@ public class StageAssistant {
     private static StageAssistant instance;
     private StageAssistant() {}
     
-    /**
-     * Singleton pattern. Method to get instance of WindowManager, if instance is null, new instance will be created.
-     * 
-     * @return Instance of WindowManager.
-     */
     public static StageAssistant getInstance() {
         if (instance == null) {
             instance = new StageAssistant();
@@ -31,18 +26,6 @@ public class StageAssistant {
         return instance;
     }
     
-    /**
-     * Open a new fxml window called "Stage" and assign a scene to it. Scene is created under FXMLLoader class by 
-     * assigning path to .fxml file. New window can be "modal", which means that primary window is not available while
-     * showing second window.
-     * 
-     * @param fxmlPath  Path to FXML file.
-     * @param title Title of new window.
-     * @param isModal  Specifies whether the window should be modal.
-     * @param <T> Controller class assigned to input fxml file.
-     * @return Controller of new opened window assigned to input fxml file.
-     * @throws IOException if FXML file wasn't loaded correctly.
-     */
     public <T> T openNewStage(String fxmlPath, String title, boolean isModal, 
             double minWidth, double minHeight) throws IOException 
     {        
@@ -62,16 +45,6 @@ public class StageAssistant {
         return loader.getController();
     }
     
-     /**
-     * Switch Scene in current Stage that is correlated with event that occured. 
-     * Stage is accessed by event.getSource() method.
-     * 
-     * @param fxmlPath  Path to FXML file.
-     * @param event ActionEvent from which Stage can be loaded.
-     * @param <T> Controller class assigned to input fxml file.
-     * @return Controller of new opened window assigned to input fxml file.
-     * @throws IOException if FXML file wasn't loaded correctly.
-     */
     public <T> T switchScene(String fxmlPath, ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
         Parent root = loader.load();
